7ccf492645e0d0a8b0365f37813146594101b82e
yoalmog <38854216+yoalmog@users.noreply.github.com>
2026-04-05 12:00:14 -0500

    Add App component with Builder integration

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Builder from "./Builder";
+
+export default function App() {
+  return (
+    <div style={{ height: "100vh" }}>
+      <Builder />
+    </div>
+  );
+}
